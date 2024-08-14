6aa641e15416ba53b7efb018d9a2932942fe2194
Nicholas <nicholasleeeman@gmail.com>
2024-08-14 20:11:56 +1000

    deleted src/app

--- backend/Models/User.cs
+++ backend/Models/User.cs
@@ -5,8 +5,6 @@ public class User
     public string Name { get; set; }
     public Guid UserID { get; set; }
     
-    // EF Core requires a parameterless constructor
-    public User() { }
 
     // Optional: Additional constructor for your custom logic
     public User(Guid userID, string name)
