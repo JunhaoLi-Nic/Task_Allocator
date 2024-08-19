85ef1b478d271cfeae3ee0d1e5b46839603d2af9
Nicholas <nicholasleeeman@gmail.com>
2024-08-19 11:07:11 +1000

    delayed exit button, solve bug during transition

--- frontend/app/ui/card.tsx
+++ frontend/app/ui/card.tsx
@@ -1,5 +1,5 @@
 import { motion, AnimatePresence } from 'framer-motion';
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 interface CardProps {
@@ -12,6 +12,14 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ item, isOpen, isDimmed, onClick }) => {
 
     const [hover, setHover] = useState(false);
+    const [showExitButton, setShowExitButton] = useState(false);
+
+    const handleExitClick = () => {
+        setTimeout(() => {
+            setShowExitButton(true);
+        }, 1000);
+    };
+
 
     return (
         <AnimatePresence>
@@ -20,6 +28,7 @@ const Card: React.FC<CardProps> = ({ item, isOpen, isDimmed, onClick }) => {
                 onClick={(e: React.MouseEvent<HTMLDivElement>) => {
                     if (!isOpen) {
                         onClick();
+                        handleExitClick();
                     }
                     e.stopPropagation(); // Prevents closing when opening the card
                 }}
@@ -36,7 +45,7 @@ const Card: React.FC<CardProps> = ({ item, isOpen, isDimmed, onClick }) => {
                     ease: 'easeInOut'
                 }}
                 whileHover={{ scale: 1.1 }}
-                whileTap={{ scale: 0.9 }}
+                whileTap={{ scale: 1}}
                 style={{
                     backgroundColor: 'white',
                     display: 'flex',
@@ -49,6 +58,7 @@ const Card: React.FC<CardProps> = ({ item, isOpen, isDimmed, onClick }) => {
                     top: isOpen ? '30%' : undefined,
                 }}
                 onMouseLeave={() => setHover(false)}  //init hover
+
             >
                 <motion.h2>Framer Motion</motion.h2>
                 {isOpen && (
@@ -58,38 +68,44 @@ const Card: React.FC<CardProps> = ({ item, isOpen, isDimmed, onClick }) => {
                         initial={{ opacity: 0 }}
                         animate={{ opacity: 1 }}
                         exit={{ opacity: 0 }}
+                        
                     >
                         <p className="p-30 leading-10">Animating Box</p>
 
-                        <div
-                            style={{        
-                                position: 'absolute',
-                                top: '20px', 
-                                right: '20px',
-                                cursor: 'pointer',
-                                width: '30px',
-                                height: '30px',
-                                borderRadius: '50%',
-                                border:"none",
-                                display: 'flex',
-                                alignItems: 'center',
-                                justifyContent: 'center',
-                                backgroundColor: hover ? '#A3A3A3' : '#F6F2F2', // Darker when hovered
+                        {showExitButton  && (
+                            <div
+                                style={{
+                                    position: 'absolute',
+                                    top: '20px',
+                                    right: '20px',
+                                    cursor: 'pointer',
+                                    width: '30px',
+                                    height: '30px',
+                                    borderRadius: '50%',
+                                    border: "none",
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                    backgroundColor: hover ? '#A3A3A3' : '#F6F2F2', // Darker when hovered
                                 }}
 
-                            onMouseEnter={() => setHover(true)}
-                            onMouseLeave={() => setHover(false)}
-                            
-                            onClick={(e) => {
-                                e.stopPropagation(); // Prevents the card from toggling open when closing
-                                onClick();
-                            }}
-                        >
-                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M18 6L6 18" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                                <path d="M6 6L18 18" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                            </svg>
-                        </div>
+                                onMouseEnter={() => setHover(true)}
+                                onMouseLeave={() => setHover(false)}
+
+                                onClick={(e) => {
+                                    e.stopPropagation(); // Prevents the card from toggling open when closing
+                                    onClick();
+                                    setShowExitButton(false);
+                                }}
+                            >
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M18 6L6 18" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                                    <path d="M6 6L18 18" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                                </svg>
+                            </div>
+
+                        )}
+
                     </motion.div>
                 )}
             </motion.div>
